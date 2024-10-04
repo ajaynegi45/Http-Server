b1298acc7fdfbb5a1b6026313a8f85918d0d8660
Ajay Negi <97536703+ajaynegi45@users.noreply.github.com>
2024-10-04 13:16:37 +0530

    Update the server configuration

--- src/main/java/com/httpserver/HttpServerApplication.java
+++ src/main/java/com/httpserver/HttpServerApplication.java
@@ -1,7 +1,10 @@
 package com.httpserver;
 
 import org.slf4j.Logger;
+
 import java.io.IOException;
+import java.util.Objects;
+
 import org.slf4j.LoggerFactory;
 import com.httpserver.config.Configuration;
 import com.httpserver.core.ServerListenerThread;
@@ -16,39 +19,43 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HttpServerApplication {
 
-	private final static Logger LOGGER = LoggerFactory.getLogger(HttpServerApplication.class);
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpServerApplication.class);
 
-	/**
-	 * The entry point of the HTTP server application.
-	 *
-	 * @param args command-line arguments; expects a single argument specifying the configuration file path.
-	 */
-	public static void main(String[] args) {
+    /**
+     * The entry point of the HTTP server application.
+     *
+     * @param args command-line arguments; expects a single argument specifying the configuration file path.
+     */
+    public static void main(String[] args) {
 
-		if (args.length != 1) {
-			LOGGER.error("No configuration file provided.");
-			LOGGER.error("Syntax:  java -jar simplehttpserver-1.0-SNAPSHOT.jar  <config.json>");
-			return;
-		}
+//		if (args.length != 1) {
+//			LOGGER.error("No configuration file provided.");
+//			LOGGER.error("Syntax:  java -jar simplehttpserver-1.0-SNAPSHOT.jar  <config.json>");
+//			return;
+//		}
 
-		LOGGER.info("Starting server...");
+        LOGGER.info("Starting server...");
 
-		// Load the configuration from the provided JSON file.
-		ConfigurationManager.getInstance().loadConfigurationFile(args[0]);
-		Configuration config = ConfigurationManager.getInstance().getCurrentConfiguration();
+        try {
+            // Load the configuration from the provided JSON file.
+            ConfigurationManager.getInstance().loadConfigurationFile(Objects.requireNonNull(HttpServerApplication.class.getClassLoader().getResource("http.json")).getFile());
+        } catch (Exception e) {
+            LOGGER.error("Failed to load configuration file: {}", e.getMessage());
+            return;
+        }
+        Configuration config = ConfigurationManager.getInstance().getCurrentConfiguration();
 
-		LOGGER.info("Using Port: {}", config.getPort());
-		LOGGER.info("Using Webroot: {}", config.getWebroot());
+        LOGGER.info("Using Port: {}", config.getPort());
+        LOGGER.info("Using Webroot: {}", config.getWebroot());
 
-		try {
-			System.out.println("Thread Starting...");
-			ServerListenerThread serverListenerThread = new ServerListenerThread(config.getPort(), config.getWebroot());
-			serverListenerThread.start();
-
-			System.out.println("Thread Ended");
-		} catch (IOException e) {
-			// TODO Handle Later
-			e.printStackTrace();
-		}
-	}
+        try {
+            LOGGER.info("Starting server listener thread...");
+            ServerListenerThread serverListenerThread = new ServerListenerThread(config.getPort(), config.getWebroot());
+            serverListenerThread.start();
+            LOGGER.info("Server listener thread started successfully.");
+        } catch (IOException e) {
+            // TODO Handle Later
+            LOGGER.error("Error starting server listener thread: {}", e.getMessage());
+        }
+    }
 }
